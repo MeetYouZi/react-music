c2f94da7648903c95e41ba0ed201ca76eb9d6b2b
meetyouzi <987176347@qq.com>
2020-06-11 17:56:53 +0800

    推荐列表请求数据展示

--- src/pages/recommend/index.js
+++ src/pages/recommend/index.js
@@ -3,7 +3,6 @@ import { connect } from 'react-redux'
 import './style.styl'
 import { withRouter } from 'react-router-dom'
 import { getToplist } from '@/api'
-import './style.styl'
 
 const Recommend = () => {
   const [topList, setTopList] = useState([])
@@ -37,11 +36,27 @@ const Recommend = () => {
         <div className="rank_title">官方榜</div>
         <ul className="rank_list_top">
           {
-            topList.length && topList.forEach((song, index) => {
+            topList.length && topList.map((item, index1) => {
               return (
-                <li className="rank_song_list_item" key={song.id}>
-                  <strong>{index + 1}.</strong>
-                  {song.first}-{song.second}
+                <li className="rank_song_list_item" key={item.id}>
+                  <div className="ank_list_left">
+                    <h2 className="rank_list_tit">{item.name}</h2>
+                    <ol className="rank_song_list">
+                      {
+                        item.tracks.map((sing, index) => {
+                          return (
+                            <li className="rank_song_list_item" key={index}>
+                              <strong>{index + 1}.</strong>
+                              {sing.first}-{sing.second}
+                            </li>
+                          )
+                        })
+                      }
+                    </ol>
+                  </div>
+                  <div className="rank_list_media">
+                    <img className="rank_list_img" src={`${item.coverImgUrl}?param=100y100`}/>
+                  </div>
                 </li>
               )
             })
